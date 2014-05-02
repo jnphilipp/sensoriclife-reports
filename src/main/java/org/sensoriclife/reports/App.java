07e6df90e87bce3c8ce9d1512cb45e2a5fb85e87
jnphilipp <jnphilipp@gmail.com>
2014-05-02 18:43:31 +0200

    Init reports.

---
+++ src/main/java/org/sensoriclife/reports/App.java
@@ -0,0 +1,14 @@
+package org.sensoriclife.reports;
+
+import org.sensoriclife.Logger;
+
+/**
+ *
+ * @author jnphilipp
+ * @version 0.0.1
+ */
+public class App {
+	public static void main(String[] args) {
+		Logger.getInstance();
+	}
+}
