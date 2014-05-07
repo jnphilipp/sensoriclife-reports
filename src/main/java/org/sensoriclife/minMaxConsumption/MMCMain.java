c9acd7d7c52070aa2818c9e3d2e7ad0f7e921ae2
Marcel <m.jacob@hotmail.com>
2014-05-07 16:37:05 +0200

    adaption for map reduce main

--- src/main/java/org/sensoriclife/minMaxConsumption/MMCMain.java
+++ src/main/java/org/sensoriclife/minMaxConsumption/MMCMain.java
@@ -1,17 +1,82 @@
 package org.sensoriclife.minMaxConsumption;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
+import org.apache.accumulo.core.cli.ClientOnRequiredTable;
+import org.apache.accumulo.core.client.Connector;
 import org.apache.accumulo.core.client.mapreduce.AccumuloInputFormat;
-import org.apache.accumulo.core.client.mapreduce.AccumuloOutputFormat;
-import org.apache.accumulo.core.security.Authorizations;
+import org.apache.accumulo.core.util.CachedConfiguration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.sensoriclife.db.Accumulo;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.sensoriclife.reports.world.ResidentialUnit;
 
-public class MMCMain {
-	public static void main(String[] args) {
+import com.beust.jcommander.Parameter;
+/**
+* A simple map reduce job that computes the unique column families and column qualifiers in a table. This example shows one way to run against an offline
+* table.
+*/
+public class MMCMain extends Configured implements Tool {
+	
+	private static final Text EMPTY = new Text();
 
+	static class Opts extends ClientOnRequiredTable {
+		@Parameter(names = "--output", description = "output directory")
+		String output;
+		@Parameter(names = "--reducers", description = "number of reducers to use", required = true)
+		int reducers;
+		@Parameter(names = "--offline", description = "run against an offline table")
+		boolean offline = false;
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+
+		Opts opts = new Opts();
+		opts.parseArgs(MMCMain.class.getName(), args);
+		String jobName = this.getClass().getSimpleName() + "_" + System.currentTimeMillis();
+		Job job = Job.getInstance(getConf(), jobName);
+		job.setJarByClass(this.getClass());
+		String clone = opts.tableName;
+		Connector conn = null;
+		opts.setAccumuloConfigs(job);
+		if (opts.offline) {
+			conn = opts.getConnector();
+			clone = opts.tableName + "_" + jobName;
+			conn.tableOperations().clone(opts.tableName, clone, true, new HashMap<String,String>(), new HashSet<String>());
+			conn.tableOperations().offline(clone);
+			AccumuloInputFormat.setOfflineTableScan(job, true);
+			AccumuloInputFormat.setInputTableName(job, clone);
+		}
+		job.setInputFormatClass(AccumuloInputFormat.class);
+		job.setMapperClass(AccumuloMMCMapper.class);
+		job.setMapOutputKeyClass(NullWritable.class);
+		job.setMapOutputValueClass(ResidentialUnit.class);
+		job.setCombinerClass(AccumuloMMCReducer.class);
+		job.setReducerClass(AccumuloMMCReducer.class);
+		job.setNumReduceTasks(opts.reducers);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		TextOutputFormat.setOutputPath(job, new Path(opts.output));
+		job.waitForCompletion(true);
+
+		if (opts.offline) {
+			conn.tableOperations().delete(clone);
+		}
+		return job.isSuccessful() ? 0 : 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(CachedConfiguration.getInstance(), new MMCMain(), args);
+		
+		
+		/*
+		
 		Job job = new Job(getConf());
 		AccumuloInputFormat.setInputInfo(job,
 		        "user",
@@ -43,10 +108,10 @@ public class MMCMain {
 		accumulo.scanByKey("electricity_consumption", key, attribute, authentication);
 		//start job (including map and reduce -> with report)
 		//start bin/tool.sh script
+		*/
 		
-	}
-	
-	public void getConf(){
+
 		
+		System.exit(res);
 	}
 }
